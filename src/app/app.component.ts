4436ff2a4e650b26dc3945b8a32ffb8298e7d965
loveheenavasu <trivediheenalove@gmail.com>
2024-01-10 17:28:31 +0530

    fix:changed button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {
-  SocialLoginModule,
-  SocialAuthServiceConfig,
-  SocialAuthService,
-  SocialUser,
-} from '@abacritt/angularx-social-login';
+import { SocialLoginModule, SocialAuthServiceConfig, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import {
   GoogleLoginProvider,
-  FacebookLoginProvider,
+  FacebookLoginProvider
 } from '@abacritt/angularx-social-login';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-declare const google: any;
+declare var google:any;
 declare const gapi: any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SocialLoginModule],
+  imports: [CommonModule, RouterOutlet , SocialLoginModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+ 
 })
 export class AppComponent {
   loginForm!: FormGroup;
@@ -41,46 +37,31 @@ export class AppComponent {
       this.socialUser = user;
       this.isLoggedin = user != null;
     });
+
+    gapi.load('auth2', () => {
+      gapi.auth2.init({
+        client_id: '745628972767-6hdrglvtjp8fi8ohq9m2qqkjv4r7st4q.apps.googleusercontent.com',
+      });
+    });
   }
 
+  
   loginWithFacebook(): void {
-    this.socialAuthService
-      .signIn(FacebookLoginProvider.PROVIDER_ID)
-      .then((res) => {
-        console.log('res');
-      });
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID).then((res)=>{
+      console.log("res")
+    });
   }
+
   loginWithGoogle(): void {
     this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
-  
-
-  // loginWithGoogle(): void {
-    // gapi.auth2
-    //   .getAuthInstance()
-    //   .signIn()
-    //   .then(
-    //     (googleUser: any) => {
-    //       // Handle successful sign-in
-    //       const idToken = googleUser.getAuthResponse().id_token;
-    //       console.log('Google User:', googleUser);
-    //     },
-    //     (error: any) => {
-    //       // Handle sign-in error
-    //       console.error('Error during Google Sign-In', error);
-    //     }
-    //   );
-  // }
-
-
+}
   signOut(): void {
     this.socialAuthService.signOut();
   }
 
-  logOut(): void {
+
+
+logOut(): void {
     this.socialAuthService.signOut();
-  }
-
-
-
+}
 }
