a725f84c5adf415f90c5d431ca3073fbb3bed3dc
loveheenavasu <trivediheenalove@gmail.com>
2024-01-10 12:43:30 +0530

    feat:fb-login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SocialLoginModule, SocialAuthServiceConfig, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from '@abacritt/angularx-social-login';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet , SocialLoginModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+ 
 })
 export class AppComponent {
-  title = 'social-login';
+  loginForm!: FormGroup;
+  socialUser!: SocialUser;
+  isLoggedin?: boolean = undefined;
+  constructor(
+    private formBuilder: FormBuilder,
+    private socialAuthService: SocialAuthService
+  ) {
+    console.log(this.isLoggedin);
+  }
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = user != null;
+    });
+  }
+  loginWithFacebook(): void {
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+  signOut(): void {
+    this.socialAuthService.signOut();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { FacebookLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration() ,   {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider('337362629218518'),
+        },
+      ],
+    } as SocialAuthServiceConfig,
+  },]
 };
